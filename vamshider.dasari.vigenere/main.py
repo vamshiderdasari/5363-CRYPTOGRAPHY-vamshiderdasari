c416c377695408ce8b71524734cc4801791893f6
vamshiderdasari <dasari.vamshi0608@gmail.com>
2015-08-04 17:43:06 -0500

    adding files

--- vamshider.dasari.vigenere/main.py
+++ vamshider.dasari.vigenere/main.py
@@ -1,9 +1,3 @@
-###############################################
-# Name: Vamshider Reddy,Dasari
-# Class: CMPS 5363 Cryptography
-# Date: 28 July 2015
-# Program 2 - Vigenere Cipher
-###############################################
 import argparse
 import sys
 import randomized_vigenere as rv
@@ -14,25 +8,20 @@ def main():
     parser.add_argument("-m", "--mode", dest="mode", default = "encrypt", help="Encrypt or Decrypt")
     parser.add_argument("-i", "--inputfile", dest="inputFile", help="Input Name")
     parser.add_argument("-o", "--outputfile", dest="outputFile", help="Output Name")
-    parser.add_argument("-s", "--seed", dest="seed",help="Integer seed")
-    parser.add_argument("-t", "--type", dest="type",help="Encoding / Decoding mode [enc,dec]")
+    parser.add_argument("-s", "--seed", type=int,help="Integer seed")
 
     args = parser.parse_args()
 
-    folder = open(args.inputFile,'r')
-    message = folder.read()
-    tool = open(args.seed,'r')
-    seed = tool.read()
-    #here i am taking the seed inside a file which will be more safe.
-    key = rv.keywordFromSeed(seed)
+    f = open(args.inputFile,'r')
+    message = f.read()
+    key = rv.keywordFromSeed(args.seed)
+    print(key)
     if(args.mode == 'encrypt'):
-        result = rv.encrypt(message,key,seed)
-        print("The messege has been Encrypted")
+        data = rv.encrypt(message,key,args.seed)
     else:
-        result= rv.decrypt(message,key,seed)
-        print("The messege has been Decrypted")
+        data = rv.decrypt(message,key,args.seed)
     o = open(args.outputFile,'w')
-    o.write(str(result))
+    o.write(str(data))
 
 
 if __name__ == '__main__':
